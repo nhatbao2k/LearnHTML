59287f35af8652c8f332d7009aed22a82eda8bab
2ksoft <duchanhstyle@gmail.com>
2020-10-16 00:56:25 +0700

    First commit

---
+++ HTMLElementClass/index.js
@@ -0,0 +1,4 @@
+document.createElement("li");
+var newLi = document.createElement("li");
+newLi.textContent = "four";
+li1.appendChild(newLi);
---
+++ runJavaScriptInBrown/index.js
@@ -0,0 +1,16 @@
+function Cat() {
+  this.stomach = [];
+}
+
+Cat.prototype.eat = function (mouse) {
+  this.stomach.push(mouse);
+  return this;
+};
+
+function Mouse(name) {
+  this.name = name;
+}
+
+var tom = new Cat();
+var mouse = new Mouse("Mickey");
+console.log(tom.eat(mouse));
